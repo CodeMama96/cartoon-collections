44ecec6ac70f699174610275554e0ebc1c56ff4f
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 22:34:30 -0400

    commit tree b38dc0edc124cb7dd090c4f89a391b50912b0caf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarves) # code an argument here
+  # names = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+  dwarves.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  #=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  veggies.collect do | name |
+  name.capitalize + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  #index = 0
+  if short_words.any? {|index| index.length > 4}
+    return true
+  else 
+    return false
+  index = index + 1
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(cheese) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+  cheese_types.include?(cheese)
+ # ay below is here to help
+ end
 end
